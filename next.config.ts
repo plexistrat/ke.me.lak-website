32c92a662a4d9d1ad1fca1213e388d7947aea4b6
plexistrat <drosostaikos@yahoo.gr>
2025-09-17 13:54:59 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        has: [
-          {
-            type: "host",
-            value: "ke-me-lak-website.vercel.app", // αλλάξε το με το πραγματικό vercel domain σου
-          },
-        ],
-        destination: "https://kemelak.gr/:path*",
-        permanent: true,
-      },
-      {
-        source: "/(.*)",
-        has: [
-          {
-            type: "host",
-            value: "www.kemelak.gr",
-          },
-        ],
-        destination: "https://kemelak.gr/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
